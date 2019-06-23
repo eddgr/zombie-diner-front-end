ec60efcf619c5a24605a3bda400b78ec3cfb0440
Edgar Ong <emailedgar@gmail.com>
2019-06-23 15:27:50 -0400

    added game over screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import React from 'react';
-import HomeContainer from './Containers/HomeContainer.js'
+// import HomeContainer from './Containers/HomeContainer.js'
 import GameContainer from './Containers/GameContainer.js'
 
-function App() {
+import Countdown from 'react-countdown-now';
+
+let timeMax = 20
+
+const renderer = ({ hours, minutes, seconds, completed }) => {
+  let time = (seconds/timeMax)*100
+  const countdown = {
+    width: `${time}%`
+  }
+
   return (
-    <div>
-      <GameContainer />
+    <div class="progress rounded-0">
+      <div className="progress-bar bg-danger" role="progressbar" style={countdown} aria-valuenow={seconds} aria-valuemin="0" aria-valuemax={timeMax}></div>
     </div>
-  );
+  )
+};
+
+class App extends React.Component {
+  state = {
+    gameStart: true
+  }
+
+  completeGame = () => {
+    this.setState({
+      gameStart: false
+    })
+  }
+
+  render() {
+    return (
+      <div className="text-center">
+        {
+          this.state.gameStart ?
+          <React.Fragment>
+            <Countdown
+              onComplete={() => this.completeGame()}
+              renderer={renderer}
+              date={Date.now() + timeMax * 1000} />
+            <GameContainer />
+          </React.Fragment>
+        :
+          <React.Fragment>
+            <h2 className="mt-4 text-danger text-center">Thanks for playing!</h2>
+            <a href="" className="btn btn-danger">PLAY AGAIN</a>
+          </React.Fragment>
+        }
+      </div>
+    );
+  }
 }
 
 // <HomeContainer />
